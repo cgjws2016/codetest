3f180a7ba79550395019be3d6ce6cc4bb77f42d5
Chris Grice <Chris@lipwig.(none)>
2016-05-02 21:20:17 +0100

    Added Product model

---
+++ app/Product.php
@@ -0,0 +1,48 @@
+<?php  
+
+namespace SainsBot;
+
+class Product {
+
+	private $_name;
+	private $_href;
+	private $_price;
+	private $_page_size;
+
+	public function getName(){
+        return $this->_name;
+    }
+
+    public function setName($_name){
+        $this->_name = $_name;
+        return $this;
+    }
+
+    public function getHref(){
+        return $this->_href;
+    }
+
+    public function setHref($_href){
+        $this->_href = $_href;
+        return $this;
+    }
+
+    public function getPrice(){
+        return $this->_price;
+    }
+
+    public function setPrice($_price){
+        $this->_price = $_price;
+        return $this;
+    }
+
+    public function getPageSize(){
+        return $this->_page_size;
+    }
+
+    public function setPageSize($_page_size){
+        $this->_page_size = $_page_size;
+        return $this;
+    }
+
+}
