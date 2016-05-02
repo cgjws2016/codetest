5e305debecb6a4c0ac4ee1e5da817c9f8139ed68
Chris Grice <Chris@lipwig.(none)>
2016-05-02 20:56:18 +0100

    Adding simple Product collection object

---
+++ app/Product/Collection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SainsBot\Product;
+
+use SainsBot\Product;
+
+class Collection {
+
+	private $_items;
+
+	public function add(Product $product) {
+		$this->_items[] = $product;
+	}
+
+	public function total() {
+
+	}
+
+	public function items() {
+		return $this->_items;
+	}
+
+}
