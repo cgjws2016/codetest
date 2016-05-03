893dec23d2f84193ac84bd8e2d5de0a1ca0c3637
Chris Grice <chris@chrisgrice.com>
2016-05-03 00:22:49 +0000

    Moving output handling into collection class

--- app/Product/Collection.php
+++ app/Product/Collection.php
@@ -27,4 +27,26 @@ class Collection {
 		return $this->_items;
 	}
 
+	public function toXml() {
+		return "<doc><el>test</el></doc>";
+	}
+
+	public function toJson() {
+		$output = array();
+		$output['total'] = $this->total();
+		$output['results'] = array();
+
+		foreach($this->items() as $item) {
+			$result = array();
+			$result['title'] = $item->getName();
+			$result['size'] = $item->getPageSize();
+			$result['unit_price'] = $item->getPrice();
+			$result['description'] = $item->getDescription();
+
+			$output['results'][] = $result;
+		}
+
+		return json_encode($output);
+	}
+
 }
